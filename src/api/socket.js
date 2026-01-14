bea6df5bc8218ee515e46882b0b46281247f15ac
surtr <46033510+XYN0N@users.noreply.github.com>
2026-01-14 16:28:16 +0100

    Update socket connection URL to fixed URL
    fixed the backend url

--- src/api/socket.js
+++ src/api/socket.js
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
 
-export const socket = io.connect("http://localhost:5000");
+export const socket = io.connect("https://uno-backend-web.onrender.com");
 
 socket.reconnectionAttempts = 3;
